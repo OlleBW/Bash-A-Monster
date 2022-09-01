e5034fbc799556afbcaa9b2c14efa922f6da1ca9
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-09-01 10:38:00 +0200

    .

--- Bash-A-Monster/Program.cs
+++ Bash-A-Monster/Program.cs
@@ -3,8 +3,7 @@
 
 
 Console.WriteLine("Write your name and press Enter to start the fight!");
-string playerName = Console.ReadLine();
-new Player();
-new Monster();
+Player p1 = new Player();
+Monster m1 = new Monster();
 Console.Clear();
 Console.WriteLine($"Hello {playerName} Press Enter to Punch!");
